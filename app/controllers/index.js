99639293b1a668634a875f4d14b0af6e77dc49d0
omargonzaleztecheira@gmail.com <omargonzaleztecheira@gmail.com>
2016-10-13 17:44:24 -0300

    arranque

---
+++ app/controllers/index.js
@@ -0,0 +1,9 @@
+$.imagenW.addEventListener('click', function(_evento) {
+	//navegador para ver la peli
+	Ti.Platform.openURL('https://www.youtube.com/watch?v=8TQ-gD4UCmI');
+});
+$.imagenX.addEventListener('click', function(_evento) {
+	//navegador para ver la peli
+	Ti.Platform.openURL('https://www.youtube.com/watch?v=pK2zYHWDZKo');
+});
+$.index.open();
